01e521ae598983003fdd3e3cd2f649b8c0694441
SamyBlk <samybelkessa11@gmail.com>
2023-01-27 11:04:11 +0100

    E-Books

---
+++ footer.php
@@ -0,0 +1,41 @@
+<section class="footer">
+
+   <div class="box-container">
+
+      <div class="box">
+         <h3>Liens rapides</h3>
+         <a href="home.php">Accueil</a>
+         <a href="about.php">À propos</a>
+         <a href="shop.php">shop</a>
+         <a href="contact.php">contact</a>
+      </div>
+
+      <div class="box">
+         <h3>liens supplémentaires</h3>
+         <a href="login.php">login</a>
+         <a href="register.php">register</a>
+         <a href="cart.php">panier</a>
+         <a href="orders.php">commandes</a>
+      </div>
+
+      <div class="box">
+         <h3>infos contact</h3>
+         <p> <i class="fas fa-phone"></i> +123-456-7890 </p>
+         <p> <i class="fas fa-phone"></i> +111-222-3333 </p>
+         <p> <i class="fas fa-envelope"></i> zou3ama_11@gmail.com </p>
+         <p> <i class="fas fa-map-marker-alt"></i> El Acour, Alger - 19 </p>
+      </div>
+
+      <div class="box">
+         <h3>Suivez nous</h3>
+         <a href="#"> <i class="fab fa-facebook-f"></i> facebook </a>
+         <a href="#"> <i class="fab fa-twitter"></i> twitter </a>
+         <a href="#"> <i class="fab fa-instagram"></i> instagram </a>
+         <a href="#"> <i class="fab fa-linkedin"></i> linkedin </a>
+      </div>
+
+   </div>
+
+   <p class="credit"> &copy; copyright  @ <?php echo date('Y'); ?> by <span>Mr. B. Samy & Mr. MA.Chikhi</span> </p>
+
+</section>
